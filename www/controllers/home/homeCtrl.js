06e7affab07ffd73176d94d159127846bc7a9743
Eduardo Moura <edoura@gmail.com>
2017-04-27 19:58:31 -0300

    Evolução na homepage referente aos itens da issue #27

--- www/controllers/home/homeCtrl.js
+++ www/controllers/home/homeCtrl.js
@@ -2,15 +2,75 @@
 	'use strict';
 
 	angular
-	.module('og.controllers')
-	.controller('homeCtrl', homeCtrl);
+		.module('og.controllers')
+		.controller('homeCtrl', homeCtrl);
 
-	homeCtrl.$inject = ['$http','$scope'];
+	homeCtrl.$inject = ['$http', '$scope', '$uibModal'];
 
-	function homeCtrl($http, $scope) {
+	function homeCtrl($http, $scope, $uibModal) {
 		var vm = this;
+		vm.developWithUs = developWithUs;
+		vm.spread = spread;
+		vm.foment = foment;
+
+		function developWithUs() {
+			var modalInstance = $uibModal.open({
+				templateUrl: '../../../templates/home/support-modals/develop.html',
+				size: 'md',
+				backdrop: false,
+				controller: function ($uibModalInstance) {
+					var modal = this;
+					modal.cancel = cancel;
+
+					function cancel() {
+						modalInstance.close();
+					}
+
+				},
+				controllerAs: 'modal'
+			});
+		};
+
+		function spread() {
+			var modalInstance = $uibModal.open({
+				templateUrl: '../../../templates/home/support-modals/spread.html',
+				size: 'md',
+				backdrop: false,
+				controller: function ($uibModalInstance) {
+					var modal = this;
+					modal.cancel = cancel;
+
+					function cancel() {
+						modalInstance.close();
+					}
+
+				},
+				controllerAs: 'modal'
+			});
+		};
+
+		function foment() {
+			var modalInstance = $uibModal.open({
+				templateUrl: '../../../templates/home/support-modals/foment.html',
+				size: 'md',
+				backdrop: false,
+				controller: function ($uibModalInstance) {
+					var modal = this;
+					modal.cancel = cancel;
+
+					function cancel() {
+						modalInstance.close();
+					}
+
+				},
+				controllerAs: 'modal'
+			});
+		};
+
+		// fullPage.js
 		vm.mainOptions = {
 			navigation: true,
+			anchors: ['mapa', 'queremosefazemos', 'responsavelporumaong', 'apoie', 'parceiros', 'contato'],
 			navigationPosition: 'right',
 			scrollingSpeed: 1000
 		};
