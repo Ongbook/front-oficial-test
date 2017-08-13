b07609e60873d7fa947a6f850ba34d9fba1343a9
Eduardo Moura <edoura@gmail.com>
2017-08-13 02:00:30 -0300

    Evolução na homepage - institutional ref. à issue #27

--- app/institutional/institutionalController.js
+++ app/institutional/institutionalController.js
@@ -11,60 +11,59 @@
 		var vm = this;
 		vm.authenticate = authenticate;
 		vm.showError = showError;
-		vm.developWithUs = developWithUs;
+		vm.register = register;
 		vm.spread = spread;
 		vm.foment = foment;
-		vm.register = register;
+		vm.developWithUs = developWithUs;
 
-		function register() {
-			var modalInstance = $uibModal.open({
-			 	templateUrl: 'app/institutional/register/register.html',
-			 	size:'md',
-			 	backdrop:false,
-			 	controller: function ($uibModalInstance) {
-			 		var modal = this;
-			 		modal.cancel = cancel;
-			 		modal.sendRegister = sendRegister;
-
-			 		function cancel() {
-			 			modalInstance.close();
-			 		}
-
-			 		function sendRegister(objParam) {
-			 			console.log('here');
-			 			if (objParam) {
-			 				registerServiceApi.registerUser(objParam)
-			 					.then(function (response) {
-			 						if(response.data.jwt){
-						 				modalInstance.close();
-						 				toaster.pop('success','Sucesso','Cadastro efetuado com sucesso',2000);
-					 					setTimeout(function(){
-					 						$state.go('login');
-					 					},2000);
-			 						} else {
-			 							toaster.pop('error','Dados incorretos','Revise as informações digitadas.',3000);
-			 						}
-			 					});
-			 			}
-			 		}
-			 	},
-			 	controllerAs: 'modal'
-			 });
+		function authenticate (objParam) {
+			if (objParam) {
+				loginServiceApi.auth(objParam)
+				.then(function (response) {
+					if(response.status == '200'){
+						localStorage.setItem('jwt',response.data.jwt);
+						$state.go('dashboard');
+					}
+				});
+			}
 		}
 
-		function developWithUs() {
+		function showError(val) {
+			vm.error = val;
+		}
+
+		// Modals
+		function register() {
 			var modalInstance = $uibModal.open({
-				templateUrl: '../../../templates/home/support-modals/develop/develop.html',
-				size: 'md',
-				backdrop: false,
+				templateUrl: 'app/institutional/register/register.html',
+				size:'md',
+				backdrop:false,
 				controller: function ($uibModalInstance) {
 					var modal = this;
 					modal.cancel = cancel;
+					modal.sendRegister = sendRegister;
 
 					function cancel() {
 						modalInstance.close();
 					}
 
+					function sendRegister(objParam) {
+						console.log('here');
+						if (objParam) {
+							registerServiceApi.registerUser(objParam)
+							.then(function (response) {
+								if(response.data.jwt){
+									modalInstance.close();
+									toaster.pop('success','Sucesso','Cadastro efetuado com sucesso',2000);
+									setTimeout(function(){
+										$state.go('login');
+									},2000);
+								}else {
+									toaster.pop('error','Dados incorretos','Revise as informações digitadas.',3000);
+								}
+							});
+						}
+					}
 				},
 				controllerAs: 'modal'
 			});
@@ -72,7 +71,7 @@
 
 		function spread() {
 			var modalInstance = $uibModal.open({
-				templateUrl: '../../../templates/home/support-modals/spread/spread.html',
+				templateUrl: '../../../app/institutional/support-modals/spread/spread.html',
 				size: 'md',
 				backdrop: false,
 				controller: function ($uibModalInstance) {
@@ -90,7 +89,7 @@
 
 		function foment() {
 			var modalInstance = $uibModal.open({
-				templateUrl: '../../../templates/home/support-modals/foment/foment.html',
+				templateUrl: '../../../app/institutional/support-modals/foment/foment.html',
 				size: 'md',
 				backdrop: false,
 				controller: function ($uibModalInstance) {
@@ -106,31 +105,31 @@
 			});
 		}
 
-		
-		
-		function authenticate (objParam) {
-			if (objParam) {
-				loginServiceApi.auth(objParam)
- 					.then(function (response) {
- 						if(response.status == '200'){
- 							localStorage.setItem('jwt',response.data.jwt);				 				
-		 					$state.go('dashboard');
- 						}
- 					});				 				
-			}
-		}
+		function developWithUs() {
+			var modalInstance = $uibModal.open({
+				templateUrl: '../../../app/institutional/support-modals/develop/develop.html',
+				size: 'md',
+				backdrop: false,
+				controller: function ($uibModalInstance) {
+					var modal = this;
+					modal.cancel = cancel;
 
-		function showError(val) {
-			vm.error = val;
+					function cancel() {
+						modalInstance.close();
+					}
+
+				},
+				controllerAs: 'modal'
+			});
 		}
 
 		// fullPage.js
 		vm.mainOptions = {
-			/*navigation: true,*/
-			anchors: ['mapa', 'queremosefazemos', 'responsavelporumaong', 'apoie', 'parceiros', 'contato'],
-			navigationPosition: 'right',
+			anchors: ['map', 'whant-and-make', 'ngo-owner', 'support', 'partners', 'contact'],
+			// navigation: true,
+			// navigationPosition: 'right',
 			scrollingSpeed: 1000
-		};			
+		};
 	}
 
 })();
